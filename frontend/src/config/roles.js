9e825a0dad37c531f4b15d8c8aa233669175b636
Arun Yadavalli <arunaway@berkeley.edu>
2024-09-19 21:57:08 -0700

    ready

--- frontend/src/config/roles.js
+++ frontend/src/config/roles.js
@@ -1,16 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { apiSlice } from './api/apiSlice'
-import { setupListeners } from "@reduxjs/toolkit/query"
-import authReducer from '../features/auth/authSlice'
-
-export const store = configureStore({
-    reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authReducer,
-    },
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: false
-})
-
-setupListeners(store.dispatch)
+export const ROLES = {
+    Employee: 'Employee',
+    Manager: 'Manager',
+    Admin: 'Admin'
+}
